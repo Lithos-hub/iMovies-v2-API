052bb2efb60620d75a59dcfb91d5acd586bdae20
Carlos Segura <lithos.contact@gmail.com>
2022-11-14 16:57:47 +0100

    add improvements when sending errors on signin and signup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import { registerUser, loginUser } from "../services/auth.service";
 
 const signUp = async ({ body }: Request, res: Response) => {
   const response = await registerUser(body);
   if (response === "USER_ALREADY_EXISTS") {
-    res.status(401);
-    res.send(new Error(response));
+    res.status(401).json({ error: "The user already exists" });
   } else {
     res.send(response);
   }
@@ -14,10 +13,8 @@ const signUp = async ({ body }: Request, res: Response) => {
 
 const signIn = async ({ body }: Request, res: Response) => {
   const response = await loginUser(body);
-
   if (response === "INCORRECT_PASSWORD OR USER_NOT_FOUND") {
-    res.status(406);
-    res.send(new Error(response));
+    res.status(406).json({ error: "Incorrect password or user not found" });
   } else {
     res.send(response);
   }
