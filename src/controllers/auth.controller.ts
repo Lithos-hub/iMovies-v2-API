5aaec36c76a1d8a97d1afabe9884bb06594389d3
Carlos Segura <lithos.contact@gmail.com>
2022-11-14 16:40:53 +0100

    add fix signin and signup methods

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,16 +4,20 @@ import { registerUser, loginUser } from "../services/auth.service";
 
 const signUp = async ({ body }: Request, res: Response) => {
   const response = await registerUser(body);
-  console.log("Response: ", response);
-  res.send(response);
+  if (response === "USER_ALREADY_EXISTS") {
+    res.status(401);
+    res.send(new Error(response));
+  } else {
+    res.send(response);
+  }
 };
 
 const signIn = async ({ body }: Request, res: Response) => {
   const response = await loginUser(body);
 
   if (response === "INCORRECT_PASSWORD OR USER_NOT_FOUND") {
-    res.status(403);
-    res.send(response);
+    res.status(406);
+    res.send(new Error(response));
   } else {
     res.send(response);
   }
