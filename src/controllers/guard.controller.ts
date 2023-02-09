053eb7ac6fd2b72bf7b7089849451273ce4c3b81
Carlos Segura <lithos.contact@gmail.com>
2023-02-09 08:15:43 +0100

    remove an import

--- src/controllers/guard.controller.ts
+++ src/controllers/guard.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { ExtendedRequest } from "../interfaces/request.interface";
 
 const getRoute = (req: ExtendedRequest, res: Response) => {
